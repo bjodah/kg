fe5406913df7fde2fc937e99e0883d75feaa36ea
Björn Ingvar Dahlgren <bjodah@gmail.com>
2026-08-10 12:20:01 +0200

    fix .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,7 @@
-((nil . ((eval . (let ((utils-dir (expand-file-name "utils" dir-locals-file)))
+((nil . ((eval . (let* ((root (locate-dominating-file
+                                default-directory dir-locals-file))
+                         (utils-dir (expand-file-name "utils" root)))
                    (unless (member utils-dir load-path)
                      (add-to-list 'load-path utils-dir))))
           (eval . (require 'kg-pty-recorder))
           (eval . (local-set-key (kbd "C-c r") 'kg-pty-test-record)))))
-
